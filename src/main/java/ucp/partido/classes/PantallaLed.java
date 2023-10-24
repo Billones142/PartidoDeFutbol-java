42968a0fb6e2e41ad04590f4364c5b2385c3f226
Billones142 <stefanomerinoderui@gmail.com>
2023-10-24 16:44:31 -0300

    test 6

---
+++ src/main/java/ucp/partido/classes/PantallaLed.java
@@ -0,0 +1,20 @@
+package ucp.partido.classes;
+
+import ucp.partido.interfaces.IJugador;
+
+public class PantallaLed {
+    public String imprimir(IJugador jugador) {
+        return "[Jugador "+ jugador.getNumero() +"] " + jugador.getName() +
+        " > " + jugador.getPosicion();
+    }
+
+    public String imprimir(Equipo equipo) {
+        return "[Equipo] " + equipo.getName() + " > " + equipo.getAbreviatura();
+    }
+
+    public String imprimir(Estadio estadio) {
+        return "[Estadio] " + estadio.getName() + " (" + estadio.getCiudad() +
+        " - " + estadio.getPais() + ")";
+    }
+    
+}
